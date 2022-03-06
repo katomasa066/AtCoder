b9593dbca54658a7fc24500c6fb301e9ee089ca3
katomasa066 <miya316kk@gmail.com>
2022-03-06 19:45:47 +0900

    main->Main修正

--- AC template.js
+++ AC template.js
@@ -1,17 +1,17 @@
 'use strict'
-function main(input) 
+function Main(input) 
 const number = input
 
-main(require('fs').readFileSync('/dev/stdin', 'utf8'))
+Main(require('fs').readFileSync('/dev/stdin', 'utf8'))
 
 
 
 'use strict'
-function main(input) {
+function Main(input) {
     const number = input
     const one = input.trim().split('\n');
     let number0 = one[0].split('');
     let number1 = one[1].split('');
 }
 
-main(require('fs').readFileSync('/dev/stdin', 'utf8'))
+Main(require('fs').readFileSync('/dev/stdin', 'utf8'))
